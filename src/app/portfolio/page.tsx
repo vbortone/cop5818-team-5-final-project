b6877764e2e7d45104115aae1f5dd21414c64f4f
Vincent Bortone <vbortone@gmail.com>
2024-10-16 11:50:20 -0400

    feat: add first routes

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Portfolio</h2>
+      <p>My financial recommendations</p>
+    </div>
+  );
+}
---
+++ src/app/portfolio/recommendations/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h2>Recommendations</h2>
+      <p>My financial recommendations</p>
+    </div>
+  );
+}
