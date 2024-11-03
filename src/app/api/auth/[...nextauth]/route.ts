5be4066078b6efbdb017a4c33160e60472546b98
Vincent Bortone <vbortone@gmail.com>
2024-11-03 13:03:04 -0500

    feat: add nextauthjs support

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+        }),
+    ],
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
